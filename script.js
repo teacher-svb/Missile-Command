09b8cf45c865d90f41d00c27d57e368fafc38e4a
Sam Van Battel <teacher.svb@gmail.com>
2021-01-19 08:23:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let player = null;
-let playerSpeed = 5;
+let playerSpeed = 10;
+let explosions = [];
+let explosionLife = 100;
+let shootTimer = 0;
+let shotsPerSecond = 4;
+
 
 function setup() {
     createCanvas(800, 500);
@@ -12,8 +17,39 @@ function draw() {
     background(0, 0, 0);
 
     MovePlayer();
+    Shoot();
+    RemoveDeadExplosions();
 
     drawSprites();
+}
+
+function RemoveDeadExplosions() { 
+    // kijk na of er nog explosies in de lijst zitten (lengte is groter dan 0)
+    // EN kijk na of de eerste explosie in de lijst klaar is (life is gelijk aan 0)
+    if (explosions.length > 0 && explosions[0].life == 0) { 
+        explosions.shift();     // shift() verwijdert het EERSTE item uit een lijst
+    }
+}
+
+function Shoot() {
+    shootTimer += deltaTime;
+    if (keyIsDown(32) && shootTimer >= 1000 / shotsPerSecond) {
+        CreateExplosion(player.position.x, player.position.y);
+        shootTimer = 0;
+    }
+}
+
+function CreateExplosion(x, y) {
+    let explosion = createSprite(x, y, 1, 1);
+    explosion.life = explosionLife;
+    explosion.draw = DrawExplosion;
+    explosions.push(explosion);
+}
+
+function DrawExplosion() {
+    circle(0, 0, this.width);
+    this.width++;
+    this.height++;
 }
 
 function DrawPlayer() {
